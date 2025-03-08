ea5e4a4c823c6485544d9cff0b71ab59c1b8830b
NMJ2002 <nimsarajayarathna1@gmail.com>
2025-03-08 20:11:10 +0530

    intial commit

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int m;
+    printf("Enter the size of the malloc : ");
+    scanf("%d",  &m);
+    int *pm = (int*)malloc(m * sizeof(int));
+    if (pm == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    printf("%lu", sizeof(pm));
+}
