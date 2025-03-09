553d943ce6e7422813ab64ce7be41d898f914680
NMJ2002 <nimsarajayarathna1@gmail.com>
2025-03-09 20:37:50 +0530

    stacks v1

--- stack.c
+++ stack.c
@@ -1,14 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void push(int *top1, int stack[], int value, int stack_size);
+int pop(int *top1, int stack[], int stack_size);
+int peek(int top1, int stack[], int stack_size);
+
+
 int main(void) {
-    int m;
-    printf("Enter the size of the malloc : ");
-    scanf("%d",  &m);
-    int *pm = (int*)malloc(m * sizeof(int));
-    if (pm == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
+    int stack_size;
+    int top = -1;
+    printf("Enter the stack size: ");
+    scanf("%d", &stack_size);
+    int arr[stack_size];
+    
+    for (int i = 0; i < stack_size; i++) {
+        int temp;
+        printf("Enter value %d: ", i+1);
+        scanf("%d", &temp);
+        push(&top, arr, temp,stack_size);
     }
-    printf("%llu", sizeof(pm));
+    printf("Peek: %d\n", peek(top, arr,stack_size));
+    for (int i = 0; i < stack_size; i++) {
+        int temp;
+        temp = pop(&top, arr,stack_size);
+        printf("Pop %d: %d\n", i+1, temp);
+        printf("Peeking while popping: %d\n", peek(top, arr,stack_size));
+    }
+    printf("Peekl: %d\n", peek(top, arr,stack_size));
+    return 0;
+}
+
+void push(int *top1, int stack[], int value, int stack_size) {
+    if (*top1 < stack_size && *top1 >= -1) {
+        stack[++*top1] = value;
+    } else {
+        printf("Stack Overflow!\n");
+    }
+}
+
+int pop(int *top1, int stack[], int stack_size) {
+    if (*top1 < stack_size && *top1 >= 0) {
+        return stack[(*top1)--];
+    } else if(*top1 == -1)  {
+        printf("Empty Stack!pop\n");
+        return -99;
+    } else {
+        printf("Top out of bounds!\n");
+        return -99;
+    }
+}
+
+int peek(int top1, int stack[], int stack_size) {
+    if (top1 < stack_size && top1 >= 0) {
+        return stack[top1];
+    } else if(top1 == -1)  {
+        printf("Empty Stack!peek\n");
+        return -99;
+    } else {
+        printf("Top out of bounds!\n");
+        return -99;
+    }
 }
